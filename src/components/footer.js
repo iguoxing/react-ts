bc33e393a4311568d053dd4c7c126eb39c023b68
ardenzhao <iguoxing@163.com>
2021-12-16 14:23:56 +0800

    fabric 对比度Demo

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-class Bye extends React.Component {
-    render() {
-        return <h1>Bye, {this.props.name}</h1>;
-    }
-}
-export default Bye;
-//可以导出类型组件
-// 使用this.props
