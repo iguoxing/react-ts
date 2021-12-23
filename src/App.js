5f6498273a834d8cca71a05a7b00213bcca2a138
ardenzhao <iguoxing@163.com>
2021-12-23 08:42:04 +0800

    useCallBack手写demo
    useCallBack手写demo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Welcome from './components/header.tsx'
 import Fabric from './components/Fabric'
 import Dragable from './components/Dragable'
-import HookDemo from './components/Hook'
+import HookUseCallback from './components/Hook-useCallback'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HookDemo name="Hook"></HookDemo>
+        <HookUseCallback name="Hook"></HookUseCallback>
         <Dragable name="Dragable"></Dragable>
         <Fabric name="Fabric"></Fabric>
         <Welcome name="React"></Welcome>
--- src/components/Hook-useCallback.js
+++ src/components/Hook-useCallback.js
@@ -3,23 +3,46 @@ import React, { useState,useCallback } from 'react';
 import { Button } from 'antd';
 import "antd/dist/antd.css";
 
-function HookDemo(props) {
+function HookUseCallback(props) {
   // 声明一个新的叫做 “count” 的 state 变量
-  const [count, setCount] = useState(0);
   // useCallback用法
   // 把内联回调函数及依赖项数组作为参数传入 useCallback，它将返回该回调函数的 memoized 版本，
   // 该回调函数仅在某个依赖项改变时才会更新。当你把回调函数传递给经过优化的并使用引用相等性去避免非
   // 必要渲染（例如 shouldComponentUpdate）的子组件时，它将非常有用。
   // useCallback(fn, deps) 相当于 useMemo(() => fn, deps)。
+  let lastCallback
+  let lastCallbackDependencies
+  function myClickFunction(callback,dependencies){
+    if(lastCallbackDependencies){
+        let changed = !dependencies.every((item,index)=>{
+            return item === lastCallbackDependencies[index]
+        })
+        if(changed){
+            lastCallback = callback
+            lastCallbackDependencies = dependencies
+        }
+    }else{ // 没有传入依赖项
+        lastCallback = callback
+        lastCallbackDependencies = dependencies
+    }
+    return lastCallback
+  }
+  const [count, setCount] = useState(0);
+  const [add, setAdd] = useState(0);
+  const addClickFunction = myClickFunction(()=>{console.log("addClick");setAdd(add + 1);},[])
   const clickFunction =useCallback(
     () => {
        setCount(count + 1);
-    },[count],
+    },[],
   );
 
   return (
     <div>
       <h1>Learn, {props.name}</h1>
+      <p>[myClickFunction]You clicked {add} times</p>
+      <Button type="primary" onClick={addClickFunction}>
+        Click me
+      </Button>
       <p>You clicked {count} times</p>
       <Button type="primary" onClick={clickFunction}>
         Click me
@@ -27,7 +50,7 @@ function HookDemo(props) {
     </div>
   );
 }
-export default HookDemo
+export default HookUseCallback
 
 // 参考链接
 // https://flaviocopes.com/react-hook-usecallback/
