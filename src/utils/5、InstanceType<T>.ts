a17715effe96ae22c068a530cbdc83f4818bd330
ardenzhao <iguoxing@163.com>
2022-01-15 00:03:59 +0800

    tmp

--- src/utils/5、InstanceType<T>.ts
+++ src/utils/5、InstanceType<T>.ts
@@ -2,13 +2,14 @@
  * @Author: ArdenZhao
  * @Date: 2022-01-14 19:20:06
  * @LastEditors: Do not edit
- * @LastEditTime: 2022-01-14 20:00:54
+ * @LastEditTime: 2022-01-14 23:52:18
  * @FilePath: /react-ts/src/utils/5、InstanceType<T>.ts
  * https://bigfrontend.dev/zh/typescript/InstanceType
  */
 // 对于constructor function type T，InstanceType<T> 返回其instance type。
 // 请自行实现MyInstanceType<T>。
-
+// 它的作用是获取一个类的实例类型，可以用获取到的实例类型来约束一个变量的赋值必须和类的成员类型完全一样才可以
+type InstanceType<T extends new (...args: any[]) => any> = T extends new (...args: any[]) => infer R ? R : any;
 class Foo {
   public x = '1'
   public y = 2
