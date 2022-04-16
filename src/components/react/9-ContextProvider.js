d1640b8f0931a8c9bcefaab938f369b538419d03
ardenzhao <iguoxing@163.com>
2022-04-16 16:56:33 +0800

    Update 9-ContextProvider.js

--- src/components/react/9-ContextProvider.js
+++ src/components/react/9-ContextProvider.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ArdenZhao
  * @Date: 2022-04-16 09:48:50
- * @LastEditTime: 2022-04-16 14:58:45
+ * @LastEditTime: 2022-04-16 15:14:57
  * @FilePath: /react-ts/src/components/react/9-ContextProvider.js
  * @Description: file information
  */
@@ -9,12 +9,9 @@ import React, { useState, createContext } from 'react';
 
 export const context = createContext({});
 export function ContextProvider({ children }) {
-  const [name, setName] = useState('Arden');
   const [age, setAge] = useState(18);
   const constVal = {
-    name,
     age,
-    setName,
     setAge,
     addAge: () => setAge(age + 1),
   }
